d61cf4b868b9e464eb8b6c632c4fd7bcd2d1ef9c
Josh Rifkin <Joshzrifkin@gmail.com>
2020-02-03 23:19:19 -0500

    Create assignment1_part1.py

---
+++ assignment1_part1.py
@@ -0,0 +1,21 @@
+def listDivide(numbers, divide = 2):
+    divs = len([num for num in numbers if num % divide ==0])
+	return divs
+    
+class listDivideException(Exception):
+    pass
+
+def testListDivide():
+    try:
+        listDivide([1,2,3,4,5])
+        listDivide([2,4,6,8,10])
+        listDivide([30,54,63,98,100],10)
+        listDivide([])
+        listDivide([1,2,3,4,5],1)
+        
+        except ListDivideException:
+            print("Test Case Error!") 
+            
+
+
+testListDivide()
